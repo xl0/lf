d5d5c399fda236708c9fd4ae020f995546fc1232
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2014-08-06 13:02:19 +0200

    First somewhow-working version

--- find_internship.py
+++ find_internship.py
@@ -92,6 +92,12 @@ def sum_pub_scores(pub_list):
 
 	return score
 
+def fact():
+	return {
+		'PubList' : [],
+		'Score' : 0
+	}
+
 def merge_labs(pmid_list, medline_extract):
 	cached_extract = medline_extract.distinct('_id')
 	need_to_extract = list(set(pmid_list) - set(cached_extract))
@@ -100,7 +106,8 @@ def merge_labs(pmid_list, medline_extract):
 
 	write('Merging labs from %d publications...\n' % len(pmid_list))
 	num_labs = 0
-	merged_lab_dict = {}
+	merged_lab_dict = defaultdict(dict)
+	merged_lab_dict.default_factory = fact
 	for extract in medline_extract.find({'_id' : {'$in' : pmid_list}}):
 		# Not interested in books
 		if not extract['article']:
@@ -108,48 +115,80 @@ def merge_labs(pmid_list, medline_extract):
 	
 		for author_lab in extract['AuthorLabList']:
 			num_labs += 1
-			merged_entry = {'NameVariant' : author_lab['ProcessedName'][1:],
-				'pubmed' : extract['_id'],
-				'score' : score_date(extract['PubDate'])}
+			# Use the first chunk of address as key
+			key = author_lab['ProcessedName'][0]
+			pub_score = score_date(extract['PubDate'])
+			merged_entry = {'LabName' : tuple(author_lab['ProcessedName']),
+				'PMID' : extract['_id'],
+				'Score' : pub_score,
+				'Title' : extract['Title'],
+				'Author' : author_lab['Author'],
+				'Authors' : author_lab['Authors']}
 
-			merged_lab_dict[author_lab['ProcessedName'][0]] = merged_entry
+			merged_lab_dict[key]['PubList'].append(merged_entry)
+			merged_lab_dict[key]['Score'] += pub_score
 	write('Found %d labs\n' % num_labs)
 	return merged_lab_dict	
 
-'''
-	merged_dict = defaultdict(list)
-	merged_dict.default_factory = fact
 
-	for lab_key in lab_dict.keys():
-		lab_name = sanitize_lab_name(lab_key)
-		# Determine the separator they use
-		comas = lab_name.count(',')
-		semicol = lab_name.count(';')
-#		print lab_key
-#		print comas, semicol
-		if comas >= semicol:
-			tmp_lab_name_array = lab_name.split(',')
-		else:
-			tmp_lab_name_array = lab_name.split(';')
+def show_labs(merged_lab_dict, sorted_list):
+	write('<ol>\n')
+	for lab_cluster in sorted_list:
+		split_lab_dict = defaultdict(list)
+		pub_list = merged_lab_dict[lab_cluster]['PubList']
 
-		lab_name_array = []
-		# Now fix up the names - clean up and do replacements
-		for tmp in tmp_lab_name_array:
-			tmp = tmp.strip()
-			tmp = heuristic_fix(tmp, replacement_dict)
-			lab_name_array.append(tmp)
+		for pub in pub_list:
+			entry = {
+				'PMID' : pub['PMID'],
+				'Title' : pub['Title'],
+				'Author' : pub['Author'],
+				'Authors' : pub['Authors']
+			}
+			split_lab_dict[pub['LabName']].append(entry)
+		
+		write('<li>' + lab_cluster + '\n')
+		write('<ul>')
+		for lab_name in split_lab_dict.keys():
+			write('<li>%s<ul>\n' % ', '.join(lab_name))
+			for publication in split_lab_dict[lab_name]:
+				pub_string = ''
+				for author in publication['Authors']:
+					if author == publication['Author']:
+						pub_string += "<b>" + author + " </b>"
+					else:
+						pub_string += author + ' '
+				pub_string += '<a href=\"http://www.ncbi.nlm.nih.gov/pubmed/%s\">' % publication['PMID']
+				pub_string += publication['Title'] + '</a>\n'
+				write('<li>' + pub_string.encode('utf-8') + '</li>')
+			write('</ul></li>')
+		write('</ul></li>')
+
+	write('</ol>')
+
+#			print ', '.join(lab_name)
+#			print split_lab_dict[lab_name]
+
+	
+
+#def split_labs(merged_lab_dict):
+#	split_lab_dict = defaultdict(dict)
+#	for key in merged_lab_dict.keys():
+#		labs = merged_lab_dict[key]
+#		write('key ' + str(key) + str(labs) + '\n')
+#		variants = defaultdict(list)
+#		for lab in labs:
+#			new_entry = {
+#				'pubmed' : lab['pubmed']
+#			}
 
 
-		new_entry = { 'NameVariant' : lab_name_array[1:],
-				'RawName' : lab_key,
-				'Publications' : lab_dict[lab_key],
-				'Score' : sum_pub_scores(lab_dict[lab_key])
-				}
-		merged_dict[lab_name_array[0]]['Variants'].append(new_entry)
-		merged_dict[lab_name_array[0]]['Score'] += new_entry['Score']
 
-	return merged_dict
-'''
+#			print lab
+#			split_la_entry = {'pubmed' : lab}
+#			split_lab_dict[key].append()
+		
+
+
 def fetch_me_data(record_list, raw_medline_col):
 	
 	cached_medline = raw_medline_col.distinct('_id')
@@ -230,11 +269,11 @@ def get_medline_author_list(entry):
 				for author in authors:
 					if author.has_key('Affiliation'):
 						lab = author['Affiliation']
-						forename = author.get('Forename', '')
+						forename = author.get('ForeName', '')
 						last_name = author.get('LastName', '')
 #						write('author = ' + forename + last_name + '\n')
 			
-						author_list.append((forename + last_name, lab))
+						author_list.append((forename + ' ' + last_name, lab))
 	return author_list
 
 def month_to_int(month):
@@ -283,16 +322,18 @@ def score_citation(entry):
 replacement_dict = [
 	('[De]ept\.?', 'Department'),
 	('[Ll]ab\.?\s', 'Laboratory '),
-	('\s&\s', ' and ')
+	('\s&\s', ' and '),
+	('Electronic address', '')
 ]
 
 def heuristic_fix(string, dictionary):
+	string = string.strip()
 	for entry in dictionary:
 		n_string = re.sub(entry[0], entry[1], string)
 #		if not n_string == string:
 #			print string
 #			print n_string
-		string = n_string
+		string = n_string.strip()
 	return string
 
 def extract_data(pmid_list, raw_medline, medline_extract):
@@ -306,7 +347,6 @@ def extract_data(pmid_list, raw_medline, medline_extract):
 		n = 0
 		write('need to extract: ' +  str(need_to_extract) + '\n')
 		for data in raw_medline.find({'_id' : {'$in' : need_to_extract}}):
-			write('analyzing ' +  str(data['_id']) + '\n')
 			if not n % 1000:
 				write(str(n) + '\n')
 			n += 1
@@ -320,7 +360,11 @@ def extract_data(pmid_list, raw_medline, medline_extract):
 
 			entry_list = []
 			author_list = get_medline_author_list(entry)
-			write('author list = ' + str(len(author_list)) + '\n')
+			# Full author list is saved for each lab entry's publications
+			full_author_list = []
+			for author in author_list:
+				full_author_list.append(author[0])
+
 			for author in author_list:
 				lab_name = sanitize_lab_name(author[1])
 				# Determine the separator they use
@@ -336,12 +380,14 @@ def extract_data(pmid_list, raw_medline, medline_extract):
 				for tmp in tmp_lab_name_array:
 					tmp = tmp.strip()
 					tmp = heuristic_fix(tmp, replacement_dict)
+					tmp = tmp.strip()
 					lab_name_array.append(tmp)
 
 
 				new_entry = { 'ProcessedName' : lab_name_array,
 						'RawName' : author[1],
-						'Author' : author[0]
+						'Author' : author[0],
+						'Authors' : full_author_list
 				}
 				entry_list.append(new_entry)
 			pubdate = get_medline_date(entry)
@@ -360,6 +406,7 @@ def extract_data(pmid_list, raw_medline, medline_extract):
 
 def run_search(request, request_cache):
 
+	pmid_list = []
 	res = request_cache.find_one(request)
 	if res:
 		pmid_list = res['pmid_list']
@@ -367,7 +414,7 @@ def run_search(request, request_cache):
 		write('Request found in cache.\n')
 		write('%d PMIDs to consider\n' % num_records)
 	else:
-		write('Let me ask NCBI...')
+		write('Let me ask NCBI...\n')
 
 		# Do the request
 		n = 0
@@ -377,15 +424,16 @@ def run_search(request, request_cache):
 			except urllib2.HTTPError:
 				n += 1
 				if (n > 3):
-					write('NCBI is not happy, giving up')
+					write('\nNCBI is not happy, giving up.\n')
 					return None
 
-				write('Oops\n')
+				write('Request to NCBI failed, trying again...\n')
 				continue
 			break
 
 
 		records = Entrez.read(handle)
+		write(str(records) + '\n')
 		num_records = int(records['Count'])
 		webenv = records['WebEnv']
 		query_key = records['QueryKey']
@@ -393,7 +441,6 @@ def run_search(request, request_cache):
 		write("Done. %d articles to consider.\n" % num_records)
 		write("Getting the PMIDs...\n")
 		n = 0
-		pmid_list = []
 		for start in range(0, num_records, search_chunk_size):
 			end = min(start+search_chunk_size, num_records)
 			while True:
@@ -432,7 +479,6 @@ def header(request):
 	write("<pre>\n")
 	
 def footer_good():
-	write("</pre>\n")
 	write("<p>Done!</p>\n")
 	write("</body></html>\n")
 	
@@ -457,19 +503,39 @@ def main():
 	# Avoid script injection escaping the user input
 	request = cgi.escape(request)
 
-	header(request)
+	request_escaped_quotes = cgi.escape(request, quote=True)
+	header(request_escaped_quotes)
 
 	pmid_list = run_search(request, request_cache)
 	if not pmid_list:
 		footer_bad()
 		return
+	del request_cache
 
 	res = extract_data(pmid_list, raw_medline, medline_extract)
 	if not res:
 		footer_bad()
 		return
 
-	merge_labs(pmid_list, medline_extract)
+	merged_lab_dict = merge_labs(pmid_list, medline_extract)
+#	for i in merged_lab_dict.keys():
+#		write(i + json.dumps(merged_lab_dict[i], sort_keys=True, indent=2) + '\n')
+
+
+	sorted_lab_list = sorted(merged_lab_dict, key=lambda lab_key: merged_lab_dict[lab_key]['Score'], reverse=True)
+
+	write('</pre>')
+	show_labs(merged_lab_dict, sorted_lab_list)
+
+#	for i in sorted_lab_list:
+#		print merged_lab_dict[i]['Score'], i
+#	order_list = 
+
+
+	del pmid_list
+	del medline_extract
+
+#	split_lab_dict = split_labs(merged_lab_dict)
 	footer_good()
 
 
